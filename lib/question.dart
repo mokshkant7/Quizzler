0c7e7211bc8d64f465c0d9cf9a6425525482681a
Moksh Kant <mokshkant@icloud.com>
2020-03-28 19:09:37 +0530

    completed with visibility widget

--- lib/question.dart
+++ lib/question.dart
@@ -2,9 +2,6 @@ class Question{
   String questionText;
   bool questionAnswer;
 
-  Question({String q,bool a}){        //constructor
-    questionText=q;
-    questionAnswer=a;
-  }
+   Question(this.questionText,this.questionAnswer);
 
 }
